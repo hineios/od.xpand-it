d18ca292f87190f8f2d443ac23f15d05d7bc3f1a
Fábio Vieira Almeida <fabio.vieira.almeida@tecnico.ulisboa.pt>
2016-07-08 18:13:53 +0100

    Imported original files from the Open Day coding
    These are the files of the original development at the Open Day (~1h of
    coding)

---
+++ XamarinMemeGen/XamarinMemeGen.Droid/MainActivity.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using Android.Graphics;
+
+namespace XamarinMemeGen.Droid
+{
+    [Activity(Label = "XamarinMemeGen.Droid", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        Spinner memesSpinner;
+        EditText TopText;
+        EditText BottomText;
+        Button GenerateMemeButton;
+        ImageView ResultMeme;
+
+        protected override async void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            Spinner memesSpinner = FindViewById<Spinner>(Resource.Id.spinnerMemes);
+            EditText TopText = FindViewById<EditText>(Resource.Id.TopText);
+            EditText BottomText = FindViewById<EditText>(Resource.Id.BottomText);
+            Button GenerateMemeButton = FindViewById<Button>(Resource.Id.GenerateMemeButton);
+            ImageView ResultMeme = FindViewById<ImageView>(Resource.Id.ResultMeme);
+
+            GenerateMemeButton.Click += async delegate
+            {
+                byte[] response = await Memes.GenerateMeme(memesSpinner.SelectedItem.ToString(), TopText.Text, BottomText.Text);
+                Bitmap bm = BitmapFactory.DecodeByteArray(response, 0, response.Length);
+                ResultMeme.SetImageBitmap(bm);
+            };
+
+            TopText.EditorAction
+
+            //Calls the Shared Portable Class Library to get a list with all available meme's.
+            ObservableCollection<string> memes = await Memes.GetMemesList();
+
+            //Set the list of memes to our Spinner and enable it
+            var adapter = new ArrayAdapter<String>(this, Android.Resource.Layout.SimpleSpinnerItem, memes);
+            memesSpinner.Adapter = adapter;
+
+        }
+
+        void HandleTextChanged(object sender, EventArgs e) {
+
+        }
+
+    }
+}
+
+
---
+++ XamarinMemeGen/XamarinMemeGen/Memes.cs
@@ -0,0 +1,68 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace XamarinMemeGen
+{
+    public static class Memes
+    {
+        //Gets a list of all available memes on this API
+        public static async Task<ObservableCollection<string>> GetMemesList()
+        {
+
+            var client = new HttpClient();
+
+            //headers required to call the service (API key and Accept type)
+            client.DefaultRequestHeaders.Add("X-Mashape-Key", "XBbhHT1nvvmshsTLVkHJuWlfdUepp17mN4HjsnIpb54NzH04fZ");
+            client.DefaultRequestHeaders.Add("Accept", "text/plain");
+
+            //Actually calls the service and returns a json string
+            string response = await client.GetStringAsync("https://ronreiter-meme-generator.p.mashape.com/images");
+
+            //converts json string into a ObservableCollection of strings
+            var tmpList = JsonConvert.DeserializeObject<ObservableCollection<string>>(response);
+
+            Regex regexFirstCharIsDigit = new Regex(@"\d");
+            ObservableCollection<string> memesList = new ObservableCollection<string>();
+
+            //This API has some garbage, so let's clean some of the bad items
+            foreach (string item in tmpList.OrderBy(i => i))
+            {
+                if (!string.IsNullOrWhiteSpace(item) && !regexFirstCharIsDigit.IsMatch(item)) //is item is not null or whitespace and doesn't have digits then add to list
+                {
+                    memesList.Add(item);
+                }
+            }
+            return memesList;
+        }
+
+        //Given a meme, top and bottom texts this will return an image
+       public static async Task<byte[]> GenerateMeme(string meme, string topText, string bottomText)
+        {
+
+            var client = new HttpClient();
+
+            //headers required to call the service (API key and Accept type)
+            client.DefaultRequestHeaders.Add("X-Mashape-Key", "XBbhHT1nvvmshsTLVkHJuWlfdUepp17mN4HjsnIpb54NzH04fZ");
+            
+
+            
+ 
+            //This Meme Generator Api has a problem with non-ascii chars, so we strip them just to avoid it crashing.
+            bottomText = Regex.Replace(bottomText, @"[^\u0000-\u007F]", string.Empty);
+            topText = Regex.Replace(topText, @"[^\u0000-\u007F]", string.Empty);
+
+            return await client.GetByteArrayAsync("https://ronreiter-meme-generator.p.mashape.com/meme?bottom="+bottomText+"&top="+topText+"&meme="+meme);
+
+
+            //TODO: Call API to with the meme name, top text and bottom text parameters to get an image
+            //API Call "https://ronreiter-meme-generator.p.mashape.com/meme?bottom=<bottomText>&top=<topText>&meme=<meme>
+        }
+    }
+}
